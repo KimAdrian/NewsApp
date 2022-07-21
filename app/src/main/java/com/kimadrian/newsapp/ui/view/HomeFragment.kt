01e1904ee5be6daf9a2bd072f351179363e8b746
KimAdrian <adriankimutai5@gmail.com>
2022-07-22 00:39:02 +0300

    Moved api call to viewmodel and wrapped response with mutablelivedata

--- app/src/main/java/com/kimadrian/newsapp/ui/view/HomeFragment.kt
+++ app/src/main/java/com/kimadrian/newsapp/ui/view/HomeFragment.kt
@@ -1,26 +1,15 @@
 package com.kimadrian.newsapp.ui.view
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.kimadrian.newsapp.R
-import com.kimadrian.newsapp.data.model.Article
-import com.kimadrian.newsapp.data.model.NewsResponse
-import com.kimadrian.newsapp.data.repository.NewsRepository
 import com.kimadrian.newsapp.databinding.FragmentHomeBinding
 import com.kimadrian.newsapp.ui.adapter.RecyclerViewAdapter
 import com.kimadrian.newsapp.ui.viewmodel.NewsViewModel
-import kotlinx.coroutines.launch
-import retrofit2.HttpException
-import kotlin.math.log
 
 class HomeFragment : Fragment() {
 
@@ -29,22 +18,16 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        val adapter = RecyclerViewAdapter()
         binding = FragmentHomeBinding.inflate(inflater, container, false)
-        binding.progress.visibility = View.VISIBLE
-        lifecycleScope.launch {
-            try {
-                adapter.submitList(NewsRepository().getNews().body()?.articles)
-                if (adapter.itemCount>0){
+        val viewModel = NewsViewModel()
+
+        val adapter = RecyclerViewAdapter()
+        viewModel.newsArticles.observe(viewLifecycleOwner){
+            adapter.submitList(it)
+            if (adapter.itemCount>0){
                     binding.progress.visibility = View.INVISIBLE
-                }
-                //binding.statsResponse.text = NewsRepository().getNews().body()?.articles.toString()
-            }catch (e: HttpException){
-                Log.e(TAG, "HttpException: ${e.message}")
-            } catch (e: ClassCastException){
-                Log.e(TAG, "ClassCastException: ${e.message}")
             }
 
         }
--- app/src/main/java/com/kimadrian/newsapp/ui/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/kimadrian/newsapp/ui/viewmodel/NewsViewModel.kt
@@ -1,15 +1,45 @@
 package com.kimadrian.newsapp.ui.viewmodel
 
 import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.bumptech.glide.Glide.init
+import com.kimadrian.newsapp.data.model.Article
 import com.kimadrian.newsapp.data.repository.NewsRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
+
+private const val TAG = "NewsViewModel"
 
 class NewsViewModel: ViewModel() {
+
+    private val newsRepository = NewsRepository()
+    var newsArticles = MutableLiveData<List<Article>?>()
+
+    init {
+        getNews()
+    }
+
+
+    private fun getNews() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val response = newsRepository.getNews()
+                if (response.isSuccessful && response.body() != null){
+                    newsArticles.postValue(response.body()?.articles)
+                }
+            } catch (e: HttpException) {
+                Log.e(TAG, "HttpException: ${e.message}")
+            } catch (e: ClassCastException) {
+                Log.e(TAG, "ClassCastException: ${e.message}")
+            } catch (e: Exception) {
+                Log.e(TAG, "Exception: ${e.message}",)
+            }
+        }
+
+    }
+
+
 
 }
