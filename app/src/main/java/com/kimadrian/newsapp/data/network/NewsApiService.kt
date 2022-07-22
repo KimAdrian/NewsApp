d5335f0358c3c00b2bcc443b4c8419137a43f2ad
KimAdrian <adriankimutai5@gmail.com>
2022-07-22 14:17:01 +0300

    App theme improvements

--- app/src/main/java/com/kimadrian/newsapp/data/network/NewsApiService.kt
+++ app/src/main/java/com/kimadrian/newsapp/data/network/NewsApiService.kt
@@ -1,18 +1,11 @@
 package com.kimadrian.newsapp.data.network
 
-import com.kimadrian.newsapp.data.model.Article
 import com.kimadrian.newsapp.data.model.NewsResponse
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NewsApiService {
-//    @GET("/everything")
-//    suspend fun getNews(@Query("sources") sources: String,
-//                        @Query("language") language: String,
-//                        @Query("apiKey") apiKey: String): List<Article>
-
     @GET("everything")
     suspend fun getNews(@Query("sources") sources: String,
                            @Query("language") language: String,
